e0d5d20d2db3454737d1badc0def7e8e6bfb582a
Stephan Kolontay <85121881+stfn-ko@users.noreply.github.com>
2022-02-11 19:35:37 +0200

    Initial Commit

---
+++ Website/app/errors/handlers.py
@@ -0,0 +1,14 @@
+from flask import render_template, Blueprint
+from app import db
+
+errors = Blueprint('errors', __name__)
+
+@errors.app_errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+@errors.app_errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
