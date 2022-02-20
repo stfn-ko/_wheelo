fe6cc9955663a21b9b1a9ce67aa35508051d6e60
Stephan <stepan.kolontaj@gmail.com>
2022-02-20 00:30:19 +0000

    Reworked website solution and design patterns

--- Website/app/static/js/main.js
+++ Website/app/static/js/main.js
@@ -1,13 +1,3 @@
-$(window).scroll(function() {
-    if($(this).scrollTop() > 10)
-    {
-        $('.navbar-trans').addClass('afterscroll');
-    } else
-    {
-        $('.navbar-trans').removeClass('afterscroll');
-    }
-});
-
 $(window).setTimeout(function() {
     $(".alert").fadeTo(500, 0).slideUp(500, function(){
         $(this).remove(); 
