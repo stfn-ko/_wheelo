2fe60fdc67f3b24fd817baa052e4bc44cbc8e3d4
SiobhanLouise06 <56020764+SiobhanLouise06@users.noreply.github.com>
2022-04-08 16:16:50 +0100

    Updated database

--- Website/app/vehicles/routes.py
+++ Website/app/vehicles/routes.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import make, model, vehicles
+from app.models import Make, Model, Vehicle
 from app import db
 
 vehicle = Blueprint('vehicle', __name__)
 
+@vehicle.route('/vehicle/<id>', methods=['GET', 'POST'])
+def vehicle(id):
+	vehicle_to_render = Vehicle.query.get(vehicle_id)
+
+	return render_template('vehicles/car_page.html', car=vehicle_to_render)
