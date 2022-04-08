6a70e078753a783911e45154fe09934e751c2612
SiobhanLouise06 <56020764+SiobhanLouise06@users.noreply.github.com>
2022-04-08 15:45:42 +0100

    Added vehicle table to model + added popular items
    however is hardcoded because for some reason couldn't get it to be generated by the database

--- Website/app/vehicles/routes.py
+++ Website/app/vehicles/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import Vehicle
+from app.models import make, model, vehicles
 from app import db
 
 vehicle = Blueprint('vehicle', __name__)
+
